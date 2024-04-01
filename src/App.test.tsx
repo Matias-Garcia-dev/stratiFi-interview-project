622bc867adb3ca1f6e5e2e076777bdadb6061658
Matias <matiasenriquegarciadev@gmail.com>
2024-04-01 00:15:34 -0300

    Update gitignore

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './services/store/store';
 
 describe('App', () => {
   it('renders LoginPage', () => {
-    const { getByTestId } = render(<App />);
+    const { getByTestId } = render( 
+      <Provider store={store}>
+          <App />
+      </Provider>
+      );
 
     const loginPageElement = getByTestId('loginPage');
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,9 @@
 import LoginForm from '../components/LoginForm'
-import { Provider } from 'react-redux'
-import store from "../services/store/store"
 
 const LoginPage = () => {
   return (
     <div data-testid="loginPage">
-      <Provider store={store}>
       <LoginForm/>
-      </Provider>
-      
     </div>
   )
 }
